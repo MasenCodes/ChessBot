bc348082d181c41934882e7f02a2ad75746737ce
Masen Lunt <masen.lunt@gmail.com>
2021-07-28 17:37:42 -0700

    added a destructor to Scene

--- scene.cpp
+++ scene.cpp
@@ -5,11 +5,17 @@ Scene::Scene(QObject *parent) : QGraphicsScene(parent)
 
 }
 
-void Scene::createEnv() {
+void Scene::createEnv()
+{
     for (int ii = 0;ii <= 8 ;ii++ ) {
         for (int jj = 0;jj <= 8 ;jj++ ) {
             addEllipse(jj * 96, ii * 96, 96, 96);
         }
     }
+
+}
+
+Scene::~Scene()
+{
 
 }
