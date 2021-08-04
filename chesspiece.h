2f3eee2231b8a736fc2ce55268a44b37c386195a
Masen Lunt <masen.lunt@gmail.com>
2021-08-04 12:40:49 -0700

    fixed chesspiece

--- chesspiece.h
+++ chesspiece.h
@@ -1,13 +1,12 @@
 #ifndef CHESSPIECE_H
 #define CHESSPIECE_H
 
-#include <QGraphicsItem>
+#include <QGraphicsObject>
 
-class ChessPiece
+class ChessPiece: QGraphicsObject
 {
 public:
     ChessPiece();
-    QGraphicsPixmapItem piece;
     int weight;
     int xPos;
     int yPos;
@@ -16,7 +15,9 @@ public:
     std::string name;
     virtual void move() = 0; // needs to be overwritten specifically to the chess piece
     virtual void legalMove(int xPos, int yPos) = 0; // specific to allowable move to the given piece
+    virtual void setPicture(std::string url) = 0; // set image specific to the chess piece
     void render(QGraphicsScene scene);
+
 };
 
 #endif // CHESSPIECE_H
