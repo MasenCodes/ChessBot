095221f3ea5d94e7a82e20e524685efead3e7390
Masen Lunt <masen.lunt@gmail.com>
2021-08-03 18:07:06 -0700

    rough draft for header file is complete

--- chesspiece.h
+++ chesspiece.h
@@ -1,11 +1,22 @@
 #ifndef CHESSPIECE_H
 #define CHESSPIECE_H
 
+#include <QGraphicsItem>
 
 class ChessPiece
 {
 public:
     ChessPiece();
+    QGraphicsPixmapItem piece;
+    int weight;
+    int xPos;
+    int yPos;
+    bool isCaptured;
+    bool activelySelected;
+    std::string name;
+    virtual void move() = 0; // needs to be overwritten specifically to the chess piece
+    virtual void legalMove(int xPos, int yPos) = 0; // specific to allowable move to the given piece
+    void render(QGraphicsScene scene);
 };
 
 #endif // CHESSPIECE_H
